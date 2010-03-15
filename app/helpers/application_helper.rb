9c275f6bc192dfabedd0999f211733ed13a5bcdf
Andres Montano <amontano@virginia.edu>
2010-03-15 17:41:49 +0600

    updating kmaps engines

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,6 @@
 # Methods added to this helper will be available to all templates in the application.
 module ApplicationHelper    
   def stylesheet_files
-    super + ['tmb_implicit']
+    super + ['kmaps_implicit']
   end
 end
