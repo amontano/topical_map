a6a8232843e91bfdbf7ea371f72d537fd17b1bd0
Andres Montano <amontano@virginia.edu>
2010-03-16 14:57:56 +0600

    couldn't make app and engine both have application helper

--- app/helpers/application_helper.rb
+++
@@ -1,6 +0,0 @@
-# Methods added to this helper will be available to all templates in the application.
-module ApplicationHelper    
-  def stylesheet_files
-    super + ['kmaps_implicit']
-  end
-end
